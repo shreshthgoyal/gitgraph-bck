45678b3136a345d8d32c76ce8b03d3b17b971062
shikhar13012001 <ishgupta2015@gmail.com>
2024-03-24 16:37:39 +0530

    Added .env.example and start script

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const main = require('./data'); 
+const main = require('./data.js'); 
 const port = 3000;
 
 const app = express();
